3de18e9b6e4398d5eab5753ba1e3f2c29944da85
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-06-23 10:47:02 +0200

    feat: blazingly fast resourcepack generation

--- .github/scripts/fetcher.mjs
+++ .github/scripts/fetcher.mjs
@@ -4,7 +4,7 @@ const projects = {};
 
 function fetchAll(start, max) {
   console.log('Fetching ' + start + ' / ' + max);
-  return fetch('https://api.modrinth.com/v2/search?facets=[[%22project_type:resourcepack%22]]&limit=100&offset=' + start, {
+  return fetch('https://api.modrinth.com/v3/search?facets=[[%22project_types:resourcepack%22],[%22dependency_project_ids:fc4wBpRx%22]]&limit=100&offset=' + start, {
     headers: {
       'User-Agent': 'meowdding/website (' + atob('Y29udGFjdEB0aGF0Z3Jhdnlib2F0LnRlY2g=') + ')'
     }
@@ -20,10 +20,10 @@ function fetchAll(start, max) {
       data.hits.forEach((project) => {
         projects[project.project_id] = {
           slug: project.slug,
-          title: project.title,
-          description: project.description,
+          title: project.name,
+          description: project.summary,
           icon: project.icon_url,
-          latest_version: project.latest_version,
+          latest_version: project.version_id,
           downloads: project.downloads,
           organization: project.organization,
           organization_id: project.organization_id,
@@ -33,10 +33,10 @@ function fetchAll(start, max) {
       });
       return data;
     }).then(async (data) => {
-      if (data.offset + 100 >= max) return;
+      if (start + 100 >= data.total_hits) return;
 
       await timeout(1000);
-      return fetchAll(data.offset + 100, data.total_hits);
+      return fetchAll(start + 100, data.total_hits);
     });
 }
 
@@ -44,94 +44,10 @@ async function timeout(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function fetchCatharsisVersions() {
-  return fetch('https://api.modrinth.com/v2/project/catharsis', {
-    headers: {
-      'User-Agent': 'meowdding/website (' + atob('Y29udGFjdEB0aGF0Z3Jhdnlib2F0LnRlY2g=') + ')'
-    }
-  })
-    .then((response) => {
-      try {
-        return response.json();
-      } catch (e) {
-        console.error('Failed to fetch data from Modrinth API');
-        throw e;
-      }
-    })
-    .then((data => data.versions));
-}
-
-function chunk(input, size) {
-  const chunks = [];
-  for (let i = 0; i < input.length; i += size) {
-    chunks.push(input.slice(i, i + size));
-  }
-  return chunks;
-}
-
-async function fetchVersionChunk(versionChunk, catharsisVersions) {
-  return fetch('https://api.modrinth.com/v2/versions?include_changelog=false&ids=' + JSON.stringify(versionChunk), {
-    headers: {
-      'User-Agent': 'meowdding/website (' + atob('Y29udGFjdEB0aGF0Z3Jhdnlib2F0LnRlY2g=') + ')'
-    }
-  })
-    .then((response) => {
-      try {
-        return response.json();
-      } catch (e) {
-        console.error('Failed to fetch data from Modrinth API');
-        throw e;
-      }
-    })
-    .then((data) => {
-      let versions = [];
-
-      data.forEach((version) => {
-        version.dependencies.forEach((element) => {
-          if (element.project_id === 'fc4wBpRx' || element.version_id in catharsisVersions) {
-            console.log(version.project_id)
-            versions.push(version.project_id);
-          }
-        });
-      });
-
-      return versions;
-    });
-}
-
-async function fetchVersions(versionIds, catharsisVersions) {
-  let versions = chunk(versionIds, 200);
-  let requires = [];
-
-  for (let versionChunk in versions) {
-    await timeout(1000);
-    console.log("Fetching chunk " + (parseInt(versionChunk) + 1) + " / " + versions.length);
-    let list = await fetchVersionChunk(versions[versionChunk], catharsisVersions);
-    requires.push(...list)
-    console.log(requires);
-  }
-
-  return requires
-}
-
 async function run() {
-  let catharsisVersions = await fetchCatharsisVersions();
   await fetchAll(0, 101);
-  let versions = [];
-  for (let projectId in projects) {
-    let project = projects[projectId];
-    versions.push(project.latest_version);
-  }
-  let requireCatharsis = await fetchVersions(versions, catharsisVersions);
 
-  let projectData = {};
-
-  for (let project in requireCatharsis) {
-    let id = requireCatharsis[project];
-    projectData[id] = projects[id];
-  }
-
-  fs.writeFileSync('./public/resourcepacks.json', JSON.stringify(projectData, null, 2));
+  fs.writeFileSync('./public/resourcepacks.json', JSON.stringify(projects, null, 2));
 }
 
 run();
