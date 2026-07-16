42a74190175cb3af4e8c2d5a6fa1023c4e748c32
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-07-16 22:10:13 +0200

    feat: mods fetcher

---
+++ .github/scripts/meowdding_mods_fetcher.mjs
@@ -0,0 +1,46 @@
+import fs from 'fs';
+
+const projects = {
+  mods: [],
+  texturepacks: []
+};
+
+function fetchAll() {
+  console.log('Fetching organization projects...');
+  return fetch('https://api.modrinth.com/v3/organization/CGW8DHEi/projects', {
+    headers: {
+      'User-Agent': 'meowdding/website (' + atob('Y29udGFjdEB0aGF0Z3Jhdnlib2F0LnRlY2g=') + ')'
+    }
+  })
+    .then((response) => {
+      try {
+        return response.json();
+      } catch (e) {
+        console.error('Failed to fetch data from Modrinth API');
+        throw e;
+      }
+    }).then((data) => {
+      data.forEach((project) => {
+        const formattedProject = {
+          slug: project.slug,
+          title: project.name,
+          description: project.summary,
+          icon: project.icon_url,
+        };
+
+        if (project.project_types.includes('resourcepack')) {
+          projects.texturepacks.push(formattedProject);
+        } else if (project.project_types.includes('mod')) {
+          projects.mods.push(formattedProject);
+        }
+      });
+    });
+}
+
+async function run() {
+  await fetchAll();
+
+  fs.writeFileSync('./public/projects.json', JSON.stringify(projects, null, 2));
+}
+
+run();
